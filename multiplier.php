5b882b80185efa784ebe2164e39a7387b1748a5e
Yigithan Dincer <2084054@talnet.nl>
2020-06-11 11:03:30 +0200

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,21 @@
+<?php  
+
+
+	// $waarde1 = 10;
+	// $waarde2 = 5;
+
+	// $optellen = $waarde1 + $waarde2;
+	// echo $optellen;
+
+
+
+
+  	$line1 = readline("Ik ga twee getallen met elkaar vermenigvuldigen ");
+    $line2 = readline("Eerste getal? ");
+    $line3 = readline("Tweede getal? ");
+    $line4 = readline("Uw resultaat is: ");
+    $uitkomst = readline($line2 * $line3);
+    readline_add_history($line);
+
+
+?>
