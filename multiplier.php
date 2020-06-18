55041a4e0b7eb4c551fdc7df75a01ebc92706081
Yigithan70 <60740198+Yigithan70@users.noreply.github.com>
2020-06-18 11:16:46 +0200

    Add files via upload

--- multiplier.php
+++ multiplier.php
@@ -1,15 +1,6 @@
 <?php  
 
 
-	// $waarde1 = 10;
-	// $waarde2 = 5;
-
-	// $optellen = $waarde1 + $waarde2;
-	// echo $optellen;
-
-
-
-
   	$line1 = readline("Ik ga twee getallen met elkaar vermenigvuldigen ");
     $line2 = readline("Eerste getal? ");
     $line3 = readline("Tweede getal? ");
